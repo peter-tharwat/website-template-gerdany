3097a2ef1acd6beed819d994ab0abc41176cb3e7
peter <ptharwat@gmail.com>
2021-08-20 18:19:19 +0200

    users

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 AOS.init();
-$('.owl-carousel').owlCarousel({
+$('.owl-carousel-3').owlCarousel({
     loop: true,
     margin: 30,
     responsiveClass: true,
@@ -16,6 +16,49 @@ $('.owl-carousel').owlCarousel({
         }
     }
 });
+$('.owl-carousel-4').owlCarousel({
+    loop: true,
+    margin: 30,
+    responsiveClass: true,
+    dots: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 1,
+        },
+        1000: {
+            items: 4,
+        }
+    }
+});
 Fancybox.bind("[data-fancybox]", {
   // Your options go here
 });
+
+window.addEventListener("scroll", (event) => {
+
+    let box = document.querySelector('#alert-message.active');
+    let height_message = box.offsetHeight;
+    let box2 = document.querySelector('#main-fixed-top');
+    let height_navbar = box2.offsetHeight;
+
+
+    
+
+
+
+    let scroll = this.scrollY;
+    console.log(scroll);
+    console.log("test"+height_message);
+    if(scroll>height_message){
+        $('#main-fixed-top').addClass("fixed-top");
+        $('#main-fixed-top').css({'top':"0px"});
+        $('#alert-message.active').fadeOut(0);
+        $('#fixing-margin-top').css({'padding-top':height_navbar+"px"});
+    }else{
+        $('#main-fixed-top').removeClass("fixed-top");
+        $('#alert-message.active').fadeIn(0);
+    }
+}); 
